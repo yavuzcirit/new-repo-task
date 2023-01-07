f476ca85005e774469802f67f2f2a16c27baa67f
Yavuz CIRIT <yavuzcirit23@gmail.com>
2023-01-08 02:20:19 +0300

    Delete Desktop/book-store directory

--- Desktop/book-store/src/services/book.service.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-
-const baseUrl = "https://openlibrary.org/search?language=eng&author=OL"
-
-export const getBook = async (id: string): Promise<any> => {
-    return (await axios.get(baseUrl+ id)).data
-  }
