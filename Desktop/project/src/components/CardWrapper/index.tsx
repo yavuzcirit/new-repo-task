8800b7d308305fcae2b0b6da6754d8c898cf5760
yavuzcirit <yavuzcirit23@gmail.com>
2023-01-08 15:33:10 +0300

    fix: on css

--- Desktop/project/src/components/CardWrapper/index.tsx
+++ Desktop/project/src/components/CardWrapper/index.tsx
@@ -13,7 +13,7 @@ const color = randomColor();
 
   return (
     <div className='card-wrapper' style={{borderTop:`4px solid ${color}`}}>
-        <div className='card-header-year'>{header}</div>
+        <div className='card-header-year'>{header  ? header : ""}</div>
         {data && data?.map((item: any,index: any) => <Card data={item} key={index}/>)}
         <div className='elements'></div>
     </div>
